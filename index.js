71989ae25b579d1f4d8fb49369c3e020a0a83640
TulupaAnton <tulupaanton2@gmail.com>
2026-02-12 23:58:26 +0200

    Learn how to add texture on any figure

--- index.js
+++ index.js
@@ -17,19 +17,55 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-//Material
-const material = new THREE.MeshBasicMaterial({ color: 'red' })
+const texture = new THREE.TextureLoader().load('img/grass.jpg')
+const textureMaterial = new THREE.MeshBasicMaterial({ map: texture })
 
+// Создаиние различніх фигур
+
+// const material = new THREE.MeshBasicMaterial({ color: 'red' })
 const geometry = new THREE.BoxGeometry()
-
-// Add cube
-const cube = new THREE.Mesh(geometry, material)
-
+const cube = new THREE.Mesh(geometry, textureMaterial)
+cube.position.set(-3, 0, 0)
 scene.add(cube)
+
+//СФЕРА
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 6, 6),
+  new THREE.MeshPhongMaterial({
+    color: 'blue',
+    emissive: 'white',
+    shininess: 100
+  })
+)
+sphere.position.set(2, 0, 0)
+scene.add(sphere)
+// Donat
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.7, 0.2, 16, 100),
+  new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+torus.position.set(2, 2, 1)
+scene.add(torus)
+
+//! TEXTURE
+// const texture = new THREE.TextureLoader().load('img/grass.jpg')
+// const textureMaterial = new THREE.MeshBasicMaterial({ map: texture })
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), textureMaterial)
+plane.position.set(-2, 2, 0)
+scene.add(plane)
 
 //Infinity render
 function animate () {
   requestAnimationFrame(animate)
+
+  sphere.rotation.x += 0.01
+  sphere.rotation.y += 0.01
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.01
 
   cube.rotation.x += 0.01
   cube.rotation.y += 0.01
